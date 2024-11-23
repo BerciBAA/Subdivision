5810c780da9012fa33b9eb952adc21196337b742
szilardguti <szilardguti@gmail.com>
2024-11-23 16:09:52 +0100

    Modify base class to have virtual functions

--- Subdivision/TriangleSubdivison.h
+++ Subdivision/TriangleSubdivison.h
@@ -8,6 +8,14 @@
 class TriangleSubdivison
 {
 
+public:
+    virtual void subdivide(Mesh* mesh) = 0;
+    TriangleSubdivison() = default;
+
+private:
+    virtual Vertex* createBoundaryVertex(HalfEdge* he, Mesh* mesh) = 0;
+    virtual Vertex* createInteriorVertex(HalfEdge* he, Mesh* mesh) = 0;
+
 protected:
 	void rebuildFace(Face* face, Mesh* mesh, std::vector<HalfEdge*>& newHalfEdges, std::vector<Face*>& newFaces, std::unordered_map<HalfEdge*, Vertex*>& edgeVertexMap);
 };
